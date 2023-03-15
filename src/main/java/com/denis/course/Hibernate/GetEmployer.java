ad6b4ea238b0021bbce13ca6d6178d862766e16e
Denis <denis.seremet@snt.md>
2023-03-15 12:46:13 +0200

    get user by id, HQL

---
+++ src/main/java/com/denis/course/Hibernate/GetEmployer.java
@@ -0,0 +1,32 @@
+package com.denis.course.Hibernate;
+
+import com.denis.course.Hibernate.Entity.*;
+import org.hibernate.*;
+import org.hibernate.cfg.*;
+
+public class GetEmployer {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+// Without session, you can't do some operations with java objects
+        try {
+            Session session = factory.getCurrentSession();
+            Employee emp = new Employee("Oleg", "Smirnov", "IT", 500);
+            session.beginTransaction();
+            session.save(emp);
+//            session.getTransaction().commit();
+
+            int myId = emp.getId();
+//            session = factory.getCurrentSession();
+//            session.beginTransaction();
+
+            Employee employee = session.get(Employee.class, myId);
+            session.getTransaction().commit();
+            System.out.println(employee);
+        } finally {
+            factory.close();
+        }
+    }
+}
---
+++ src/main/java/com/denis/course/Hibernate/HQL.java
@@ -0,0 +1,33 @@
+package com.denis.course.Hibernate;
+
+import com.denis.course.Hibernate.Entity.*;
+import org.hibernate.*;
+import org.hibernate.cfg.*;
+
+import java.util.*;
+
+public class HQL {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+// Without session, you can't do some operations with java objects
+        try {
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+
+//            List emps = session.createQuery("from Employee").getResultList(); // Get all employers
+            List emps = session.createQuery("from Employee " + "where name = 'Denis' AND salary > 200").getResultList(); // Get by name
+            System.out.println(" ");
+            for (Object e : emps) {
+                System.out.println(e);
+            }
+            System.out.println(" ");
+            session.getTransaction().commit();
+
+        } finally {
+            factory.close();
+        }
+    }
+}
